6ed9611f969e8dfc157e89d3ed9885b19e596e7e
Amine <anaqi@um6ss.ma>
2021-12-24 14:45:31 +0100

    more script stuff

--- Platformer/compile.sh
+++ Platformer/compile.sh
@@ -3,11 +3,16 @@ echo "Checking if the precompiled file exists"
 
 if test -f "$FILE"; then
     echo "$FILE exists."
+    echo "=> Compiling the file !"
     g++ -std=c++1z Main.cpp Game.cpp EnergyBar.cpp IOFile.cpp Player.cpp TileMap.cpp AudioGame.cpp HpBar.cpp prcHead.cpp Tile.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio  -g -o output 
+    echo "=> Executing the output file !"
     ./output
 else
-    echo "The precompiled file does not exist !"
+    echo "=> The precompiled file does not exist !"
+    echo "Compiling the header file !"
     g++ -std=c++1z prcHead.h
+    echo "=> Compiling the project !"
     g++ -std=c++1z Main.cpp Game.cpp EnergyBar.cpp IOFile.cpp Player.cpp TileMap.cpp AudioGame.cpp HpBar.cpp prcHead.cpp Tile.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio  -g -o output
+    echo "=> Executing the output file !"
     ./output
 fi
