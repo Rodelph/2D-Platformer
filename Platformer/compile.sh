9e90c8f3d93f1591556d8dc5aaa867332f74025a
Amine <anaqi@um6ss.ma>
2021-12-24 10:03:02 +0100

    more script stuff

--- Platformer/compile.sh
+++ Platformer/compile.sh
@@ -1,3 +1,13 @@
-g++ -std=c++1z prcHead.h
-g++ -std=c++1z Main.cpp Game.cpp EnergyBar.cpp IOFile.cpp Player.cpp TileMap.cpp AudioGame.cpp HpBar.cpp prcHead.cpp Tile.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio  -g -o output
-./output
+FILE=prcHead.h.gch
+echo "Checking if the precompiled file exists"
+
+if test -f "$FILE"; then
+    echo "$FILE exists."
+    g++ -std=c++1z Main.cpp Game.cpp EnergyBar.cpp IOFile.cpp Player.cpp TileMap.cpp AudioGame.cpp HpBar.cpp prcHead.cpp Tile.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio  -g -o output 
+    ./output
+else
+    echo "The precompiled file does not exist !"
+    g++ -std=c++1z prcHead.h
+    g++ -std=c++1z Main.cpp Game.cpp EnergyBar.cpp IOFile.cpp Player.cpp TileMap.cpp AudioGame.cpp HpBar.cpp prcHead.cpp Tile.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio  -g -o output
+    ./output
+fi
